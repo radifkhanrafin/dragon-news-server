4d2c6322ecf1b080c5047231418ccc6e36854b69
radifkhanrafin <meherafkabir@gmail.com>
2023-04-27 22:41:25 +0600

    add news api

--- index.js
+++ index.js
@@ -21,6 +21,7 @@ const cors = require('cors');
 const port = process.env.PORT || 5000;
 
 const categories = require('./data/categories.json');
+const news=require('./data/news.json')
 
 app.use(cors());
 
@@ -31,6 +32,9 @@ app.get('/', (req, res) =>{
 app.get('/categories', (req, res) =>{
     console.log(categories);
     res.send(categories);
+})
+app.get('/news' , (req , res)=>{
+    res.send(news)
 })
 
 app.listen(port, () => {
