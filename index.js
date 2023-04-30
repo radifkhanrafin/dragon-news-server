33de3e2f4156fcc2563cc18a687dc81199f6c547
radifkhanrafin <meherafkabir@gmail.com>
2023-04-30 10:22:06 +0600

    vercel add

--- index.js
+++ index.js
@@ -21,20 +21,37 @@ const cors = require('cors');
 const port = process.env.PORT || 5000;
 
 const categories = require('./data/categories.json');
-const news=require('./data/news.json')
+const news = require('./data/news.json')
 
 app.use(cors());
 
-app.get('/', (req, res) =>{
+app.get('/', (req, res) => {
     res.send('Dragon is running')
 });
 
-app.get('/categories', (req, res) =>{
-    console.log(categories);
+app.get('/categories', (req, res) => {
     res.send(categories);
 })
-app.get('/news' , (req , res)=>{
+app.get('/categories/:id', (req, res) => {
+    const id = parseInt(req.params.id)
+    console.log(id)
+    if (id == 0) {
+        res.send(news)
+    } else {
+        const selectCategories = news.filter(n => parseInt(n.category_id) === id)
+        res.send(selectCategories)
+    }
+
+})
+
+app.get('/news', (req, res) => {
     res.send(news)
+})
+app.get('/news/:id', (req, res) => {
+    const id = req.params.id
+    console.log(id)
+    const selectNews = news.find(n => n._id === id)
+    res.send(selectNews)
 })
 
 app.listen(port, () => {
